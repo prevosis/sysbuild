d54cd86ad9df32338415d0574689badba582e7d6
prevosis <aadilzbhatti@gmail.com>
2016-07-14 22:39:02 -0500

    reacting

--- react-refactor/src/components/CodeEditor/CodeEditor.js
+++ react-refactor/src/components/CodeEditor/CodeEditor.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import AceEditor from 'react-ace';
+import brace from 'brace';
+
+import 'brace/mode/c_cpp';
+import 'brace/theme/monokai';
 
 export default class CodeEditor extends React.Component {
   render() {
     return (
       <div>
-        <AceEditor />
+        <AceEditor
+          mode="c_cpp"
+          theme="monokai"/>
       </div>
     )
   }
---
+++ react-refactor/src/components/Compiler-Controls/Compiler-Controls.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class CompilerControls {
+  render() {
+    return (
+      <button type="submit" />
+    );
+  }
+}
